9f6e298f1f12026d204ecc6c052f2e794e3b1ed3
Eugenio Augusto Jimenes <eugeniojimenes@gmail.com>
2021-04-24 00:18:29 -0300

    fix rubocop offenses and finish rspec tests for Utils

--- lib/nr2w.rb
+++ lib/nr2w.rb
@@ -3,75 +3,67 @@
 require "nr2w/utils"
 
 module Nr2w
-  @str = +""
+  @output = +""
+  @notation = {
+    "3" => " thousand ",
+    "6" => " million ",
+    "9" => " billion ",
+    "12" => " trillion ",
+    "15" => " quadrillion ",
+    "18" => " quintillion ",
+    "21" => " sextillion ",
+    "24" => " septillion ",
+    "27" => " octillion ",
+    "30" => " nonillion "
+  }
 
   class << self
     include Utils
 
     def return_str
-      str = @str
-      @str = +""
+      str = @output
+      @output = +""
+      @negative = false
       str
     end
 
     # Units and dozens
     def unit_dozen_hundred(hundreds_digits)
       dozens = hundreds_digits.slice(0, 2).reverse.join.to_i
-      @str.prepend dozens_str(dozens)
+      @output.prepend dozens_str(dozens)
       return unless hundreds_digits.length > 2 && hundreds_digits[2]&.positive?
 
-      @str.prepend hundreds_str(hundreds_digits[2], dozens.positive?)
+      @output.prepend hundreds_str(hundreds_digits[2], dozens.positive?)
     end
 
     def convert(value)
       deal_with_errors(value)
+      zero_or_less(value)
 
-      # Transform into positive and set negative as true
-      @negative =
-        if value.negative?
-          value = value.abs
-          true
-        end
-
-      (0...value.digits.count).step(3) do |idx|
-        hundreds_digits = value.digits.slice(idx, 3)
+      (0...@input.digits.count).step(3) do |idx|
+        hundreds_digits = @input.digits.slice(idx, 3)
         number_notation(idx) if hundreds_digits.reverse.join.to_i.positive?
         unit_dozen_hundred(hundreds_digits)
       end
 
-      @str.prepend("minus ") if @negative
-      @str = "zero" if value.zero?
-
+      @output.prepend("minus ") if @negative
       assemble_str(return_str)
     end
 
-    def number_notation(index)
-      name =
-        case index
-        when 3
-          " thousand "
-        when 6
-          " million "
-        when 9
-          " billion "
-        when 12
-          " trillion "
-        when 15
-          " quadrillion "
-        when 18
-          " quintillion "
-        when 21
-          " sextillion "
-        when 24
-          " septillion "
-        when 27
-          " octillion "
-        when 30
-          " nonillion "
-        else
-          ""
-        end
-      @str.prepend name
+    def zero_or_less(value)
+      @input = value
+      if value.zero?
+        @output = "zero"
+        return
+      end
+      return unless value.negative?
+
+      @negative = true
+      @input = value.abs
+    end
+
+    def number_notation(value)
+      @output.prepend(@notation[value.to_s] || "")
     end
   end
 end
--- spec/modules/utils_spec.rb
+++ spec/modules/utils_spec.rb
@@ -21,12 +21,41 @@ RSpec.describe Nr2w::Utils do
   end
 
   describe "#hundreds_str" do
-    it "is expected to append 'hundred' " do
+    it "is expected to append 'hundred'" do
       expect(TestIncluder.hundreds_str(1)).to eq "one hundred"
     end
 
-    it "is expected to append 'hundred and ' " do
+    it "is expected to append 'hundred and '" do
       expect(TestIncluder.hundreds_str(3, true)).to eq "three hundred and "
+    end
+  end
+
+  describe "#assemble_str" do
+    it "is expected to include commas when have any THOUSAND WORD, except in the last position" do
+      expect(TestIncluder.assemble_str("five hundred and fifty seven thousand one"))
+        .to eq "five hundred and fifty seven thousand, one"
+    end
+
+    it "is expected to not include commas when have THOUSAND WORD in the last position" do
+      expect(TestIncluder.assemble_str("five hundred and fifty seven thousand"))
+        .to eq "five hundred and fifty seven thousand"
+    end
+
+    it "is expected to not include commas when don't have THOUSAND WORD" do
+      expect(TestIncluder.assemble_str("five hundred and fifty seven"))
+        .to eq "five hundred and fifty seven"
+    end
+  end
+
+  describe "#deal_with_errors" do
+    it "is expected to raise a error when input is not a integer" do
+      expect{ TestIncluder.deal_with_errors("not integer") }
+        .to raise_error(RuntimeError, "Not an integer")
+    end
+
+    it "is expected to raise a error when input is bigger then 2**100" do
+      expect{ TestIncluder.deal_with_errors(2**100) }
+        .to raise_error(RuntimeError, "Overflow")
     end
   end
 end
